2080b4935eedcc29af331cb8e22dfab3de9ec0fa
jh0152park <jh0152park@naver.com>
2023-06-28 15:21:04 +0900

    created react app

---
+++ styled_components_study/src/App.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+function App() {
+  return (
+    <div style={{ display: "flex" }}>
+      <div style={{ backgroundColor: "teal", width: 100, height: 100 }}></div>
+      <div style={{ backgroundColor: "yellow", width: 100, height: 100 }}></div>
+    </div>
+  );
+}
+
+export default App;
