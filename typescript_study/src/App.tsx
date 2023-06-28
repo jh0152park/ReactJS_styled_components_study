c103548d14b8f4e83e6dc4b3ae746a96fe96f6a9
jh0152park <jh0152park@naver.com>
2023-06-28 23:35:08 +0900

    added theme

--- typescript_study/src/App.tsx
+++ typescript_study/src/App.tsx
@@ -2,30 +2,20 @@ import { useState } from "react";
 import styled, { keyframes } from "styled-components";
 
 function App() {
-  const [value, setValue] = useState("");
-
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setValue(event.currentTarget.value);
-    console.log(event.currentTarget.value);
-  };
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(`input is ${value}`);
-    setValue("");
-  };
+  const Container = styled.div`
+    background-color: ${(prop) => prop.theme.backgroundColor};
+    width: 100vw;
+    height: 100vh;
+  `;
+  const H1 = styled.h1`
+    color: ${(prop) => prop.theme.textColor};
+  `;
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        ></input>
-        <button type="submit">Log In</button>
-      </form>
+      <Container>
+        <H1>Hello</H1>
+      </Container>
     </div>
   );
 }
