332805fdd101e5443836832499d38e923b8c1455
jh0152park <jh0152park@naver.com>
2023-06-28 22:53:41 +0900

    added state

--- typescript_study/src/Circle.tsx
+++ typescript_study/src/Circle.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 interface ContainerProps {
@@ -9,8 +10,11 @@ const Container = styled.div<ContainerProps>`
   width: 100px;
   height: 100px;
   background-color: ${(props) => props.backgroundColor};
-  border: 10px solid ${(props) => props.borderColor};
+  border: 1px solid ${(props) => props.borderColor};
   border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 interface CircleProps {
@@ -19,11 +23,20 @@ interface CircleProps {
 }
 
 function Circle({ backgroundColor, borderColor }: CircleProps) {
+  const [counter, setCounter] = useState<number>(0);
+
+  const onClick = () => {
+    setCounter((prev) => prev + 1);
+  };
+
   return (
     <Container
+      onClick={onClick}
       backgroundColor={backgroundColor}
       borderColor={borderColor ?? backgroundColor}
-    ></Container>
+    >
+      {counter}
+    </Container>
   );
 }
 
